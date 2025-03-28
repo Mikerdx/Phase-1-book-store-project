13ee86a9d19605a8b8898f8035d4ebd388c8ee76
mike <mike@DESKTOP-2JKV4QP.>
2025-03-28 12:40:33 +0300

    js fetch

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 const fetchBooks = (query = "javascript") => {//makes request to the IT store --displays necessary books as per the js 
     fetch(`https://api.itbook.store/1.0/search/${query}`)//sends request to the http
           .then(response => response.json())//changes the response JSON
+          .then(data => displayBooks(data.books))//data.books is assumed to be an array of book objects.
+  
+  
+          .catch(error => console.error("Error fetching books:", error));//logs the error to the console
+  };
+  
